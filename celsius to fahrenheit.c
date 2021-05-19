58962371795cfdb6d77c1802f0b65553da49f91c
TusharBT15 <84076430+TusharBT15@users.noreply.github.com>
2021-05-19 22:50:15 +0530

    Update and rename temp.c to celsius to fahrenheit.c

--- celsius to fahrenheit.c
+++ celsius to fahrenheit.c
@@ -3,10 +3,10 @@
 int main()
 {
 
-float celsius ,farenheit;
+float celsius ,fahrenheit;
 printf("ENTER THE TEMPERATURE IN DREGREE CELSIUS : ");
 scanf("%f" , &celsius);
-farenheit = (celsius * 9/5) + 32 ;
-printf("%.2f celsius = %.2f farenheit" , celsius , farenheit);
+fahrenheit = (celsius * 9/5) + 32 ;
+printf("%.2f celsius = %.2f fahrenheit" , celsius , fahrenheit);
 return 0;
 }
